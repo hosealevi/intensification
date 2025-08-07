21c030e7d38e892b710d7d186fc66f7f1402bc22
hosealevi <rakotolevi@gmail.com>
2025-08-07 11:37:39 +0300

    updating epi_curves

--- methods/epi_curves/ConfRates.py
+++ methods/epi_curves/ConfRates.py
@@ -11,7 +11,8 @@ script is meant to illustrate the workflow.
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import os,sys
+sys.path.append('/Users/hosealevi/Documents/SACEMA PMF 2025/intensification/methods')
 ## For logisitc regression
 import logistic as lr
 
@@ -25,19 +26,16 @@ if __name__ == "__main__":
 
 	## Get the cleaned up linelist, prepared in
 	## PrepareCaseDataset.py
-	ll = pd.read_pickle("..\\pickle_jar\\clean_linelist.pkl")
-	ll = ll.loc[ll["time"] >= "2009-01-01"].reset_index(drop=True)
+	ll = pd.read_excel("_data/input_linelist_MDG.xlsx")
+	ll = ll.loc[ll["time"] >= "2004-01-01"].reset_index(drop=True)
+	# keep only rows where admin1 is *not* NaN
+	ll = ll.dropna(subset=['admin1'])      # or ll.dropna(subset=['admin1'], inplace=True)
 	
-	## For use throughout
-	state_to_region = pd.read_pickle(
-					  "..\\pickle_jar\\state_to_region.pkl"
-					  ).set_index("state")["region"].astype(str)
-
 	## Choose the strata to work at
 	## "state" or "region"
-	strata = "state" ## Region level plotting will fail, but conf rate estimates will work.
-	assert (strata == "region" or strata == "state"),\
-			"'{}' is an invalid strata".format(strata)
+	strata = "admin1" ## Region level plotting will fail, but conf rate estimates will work.
+	#assert (strata == "region" or strata == "state"),\
+			#"'{}' is an invalid strata".format(strata)
 
 	## Construct a few additional features
 	## Starting with the time smoothing
@@ -50,7 +48,6 @@ if __name__ == "__main__":
 	ll["under_5"] = (ll["age"] < 5.).astype(np.float64)
 	ll["over_5"] = (ll["age"] >= 5.).astype(np.float64)
 	ll["missing_age"] = (ll["age"].isnull()).astype(np.float64)
-	
 	## And then the dose covariate
 	ll["missing_dose"] = (ll["doses"].isnull()).astype(np.float64)
 	ll["zero_dose"] = (ll["doses"] == 0).astype(np.float64)
@@ -63,65 +60,67 @@ if __name__ == "__main__":
 	
 	## Add state variables if strata == "region"
 	if strata == "region":
-		state_covs = pd.get_dummies(df["state"])
-		df = pd.concat([df,
-						state_covs[sorted(state_covs.columns)],
-						],
-						axis=1)
-	elif strata == "state":
-		state_covs = pd.DataFrame()
+			state_covs = pd.get_dummies(df["state"])
+			df = pd.concat([df,
+							state_covs[sorted(state_covs.columns)],
+							],
+							axis=1)
+	elif strata == "admin1":
+			state_covs = pd.DataFrame()
 	state_ef = list(state_covs.columns)
 
 	## Finally add the time covariates, with a little extra sauce to
-	## make sure we have a column for every time step.
+		## make sure we have a column for every time step.
 	time_covs = pd.get_dummies(df["month"]).T
 	time_covs = time_covs.reindex(time_steps).T.fillna(0.)
 	df = pd.concat([df,
-					time_covs[sorted(time_covs.columns)],
-					],
-					axis=1)
+						time_covs[sorted(time_covs.columns)],
+						],
+						axis=1)
 	time_ef = list(time_covs.columns)[1:]
 
 	## Set up the regression problem definitions
 	response = "classification"
 	fixed_ef = [
-				"intercept",
-				"one_dose",
-				"two_dose",
-				"missing_dose",
-				#"under_5",
-				"over_5",
-				#"missing_age",
-				]
+					"intercept",
+					"one_dose",
+					"two_dose",
+					"missing_dose",
+					#"under_5",
+					"over_5",
+					#"missing_age",
+					]
 	reference = ["under_5","zero_dose",time_steps[0]]
 
-	## including the RW matrix
+		## including the RW matrix
 	T = len(time_steps)-1
 	D2 = np.diag(T*[-2])+np.diag((T-1)*[1],k=1)+np.diag((T-1)*[1],k=-1)
-	#D2[0,2] = 1
+		#D2[0,2] = 1
 	D2[-1,-3] = 1
 	RW2 = ((4**4)/8.)*np.dot(D2.T,D2)
+	plt.matshow(RW2)
+	plt.show()
 
 	## Finally subset the data in fit and prediction pieces
 	cols = fixed_ef+\
-		   state_ef+\
-		   time_ef+\
-		   ["classification",strata]
+			state_ef+\
+			time_ef+\
+			["classification",strata]
 	df = df[cols]
 	fit_df = df.loc[df["classification"].notnull()].copy()
 
-	## Loop over strata and fit models
-	print("\nLooping over modeling strata = '{}'...".format(strata))
-	output = {}
-	covariances = {}
-	predictions = []
-	for name, sf in fit_df.groupby(strata):
-		
+## Loop over strata and fit models
+print("\nLooping over modeling strata = '{}'...".format(strata))
+output = {}
+covariances = {}
+predictions = []
+for name, sf in fit_df.groupby(strata):
+
 		## Subset to what we need for this strata
 		if strata == "region":
 			states = state_to_region.loc[state_to_region == name].index
 			this_reference = reference+[states[0]]
-		elif strata == "state":
+		elif strata == "admin1":
 			states = []
 			this_reference = reference.copy()
 
@@ -130,7 +129,7 @@ if __name__ == "__main__":
 		sf = sf[fixed_ef+\
 				list(states)[1:]+\
 				time_ef+\
-				[response]]
+				[response]].astype(np.float64)  # added np.flot
 
 		## Set up the full regularization matrix
 		this_strata_features = sf.columns[:-1]
@@ -144,8 +143,7 @@ if __name__ == "__main__":
 											   lam=lam)
 		result = lr.FitModel(model)
 		print("...for {}, success = {}".format(name,result.success))
-
-		## Save the output, first by putting together a
+   ## Save the output, first by putting together a
 		## dataframe of effects
 		beta_hat = result["x"]
 		beta_cov = result["hess_inv"]
@@ -170,7 +168,7 @@ if __name__ == "__main__":
 		## Get the prediction dataset
 		pred_df = df.loc[(df[strata] == name) &\
 						 (df["classification"].isnull())].copy()
-		pred_X = pred_df[this_strata_features].values
+		pred_X = pred_df[this_strata_features].values.astype(np.float64)
 
 		## Compute a prediction and store it
 		pred = lr.logistic_function(np.dot(pred_X,beta_hat))
@@ -181,25 +179,25 @@ if __name__ == "__main__":
 							columns=["conf_prob","conf_var"])
 		predictions.append(pred)
 
-	## Put together the 3 outputs - one of them is the regresion results, which
-	## you can use to visualize effect sizes, etc.
-	output = pd.concat(output.values(),keys=output.keys())
-	print("\nFinal output:")
-	print(output)
-	output.to_pickle("..\\pickle_jar\\conf_regression_parameters.pkl")
-	
+## Put together the 3 outputs - one of them is the regresion results, which
+## you can use to visualize effect sizes, etc.
+output = pd.concat(output.values(),keys=output.keys())
+print("\nFinal output:")
+print(output)
+output.to_pickle("pickle_jar/conf_regression_parameters.pkl")
+
 	## The other is the full covariance matrices, since sometimes you need
 	## the whole (gaussian approx) to the regression posterior
-	covariances = pd.concat(covariances.values(),keys=covariances.keys())
-	print("\nAssociated covariance matricies:")
-	print(covariances)
-	covariances.to_pickle("..\\pickle_jar\\conf_regression_covariances.pkl")
-	
+covariances = pd.concat(covariances.values(),keys=covariances.keys())
+print("\nAssociated covariance matricies:")
+print(covariances)
+covariances.to_pickle("pickle_jar/conf_regression_covariances.pkl")
+
 	## The third is the prediction dataset
-	predictions = pd.concat(predictions,
+predictions = pd.concat(predictions,
 							axis=0)
-	ll = pd.concat([ll,predictions],
+ll = pd.concat([ll,predictions],
 					axis=1)
-	print("\nAugmented line list...")
-	print(ll)
-	ll.to_pickle("..\\pickle_jar\\clean_linelist_regressed.pkl")
+print("\nAugmented line list...")
+print(ll)
+ll.to_pickle("pickle_jar/clean_linelist_regressed.pkl")
